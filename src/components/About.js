bed4bba338ff0df75c77f98a68f3412dc128196e
deedeeh <dinahafez.8@gmail.com>
2019-01-02 19:30:55 +0000

    Replaced about placeholder text with the app text and done few style changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Container, Header, Button } from 'semantic-ui-react'
 
 import '../css/About.css'
 
 const About = () => {
     return (
         <div>
-            <p className='app_intro'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <Link to='/questionnaire'>Start the journey now!</Link>
+            <Container>
+                <Header as='h2' textAlign='center'>Restore reminds you to take breaks regularly and much more.</Header>
+                <p className='app_intro'>Restore helps you during a busy day at work when you need to be reminded to take break, drink the required amount of water and maybe having a nice coffee. Just as simple as filling the questionnaire to know a little bit about you and your routine and few clicks and voilà.</p>
+                <Button fluid className='about_button'><Link to='/questionnaire'>Start</Link></Button>
+            </Container>
         </div>
     )
 }
